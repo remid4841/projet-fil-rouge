caf9771e41045fe36014d7c3ae10b66a59585c61
remid4841 <remi.danger@ynov.com>
2026-06-30 15:43:07 +0200

    Update main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -5,6 +5,10 @@ resource "aws_s3_bucket" "source" {
   # checkov:skip=CKV2_AWS_6: Public access block non requis pour le périmètre du projet
   # checkov:skip=CKV_AWS_144: Cross-region replication non requise
   # checkov:skip=CKV_AWS_145: Chiffrement KMS non requis
+  # checkov:skip=CKV2_AWS_61: Lifecycle configuration non requise pour ce projet
+  # checkov:skip=CKV_AWS_21: Versioning non requis
+  # checkov:skip=CKV2_AWS_62: Event notifications globales non requises
+  # checkov:skip=CKV_AWS_18: Access logging non requis
 }
 
 resource "aws_s3_bucket" "destination" {
@@ -14,4 +18,8 @@ resource "aws_s3_bucket" "destination" {
   # checkov:skip=CKV2_AWS_6: Public access block non requis pour le périmètre du projet
   # checkov:skip=CKV_AWS_144: Cross-region replication non requise
   # checkov:skip=CKV_AWS_145: Chiffrement KMS non requis
+  # checkov:skip=CKV2_AWS_61: Lifecycle configuration non requise pour ce projet
+  # checkov:skip=CKV_AWS_21: Versioning non requis
+  # checkov:skip=CKV2_AWS_62: Event notifications globales non requises
+  # checkov:skip=CKV_AWS_18: Access logging non requis
 }
