d90f993bbac9492cdec3837c5d501feafd00c018
sdelory-ynov <sebastien.delory@ynov.com>
2026-06-30 15:12:57 +0200

    Update main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "source" { 
-  bucket = "source-image-fadi-azzouz-2025"  
+resource "aws_s3_bucket" "source" {
+  bucket = "source-image-fadi-azzouz-2025"
   force_destroy = true 
 }
 
 resource "aws_s3_bucket" "destination" {
-  bucket = "dest-pdf-fadi-azzouz-2025" 
+  bucket = "dest-pdf-fadi-azzouz-2025"
   force_destroy = true
 }
