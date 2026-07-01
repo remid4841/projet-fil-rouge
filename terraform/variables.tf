e7db7ce2f3270ce2f8cc7881bc8f8491b2ebf0db
remid4841 <remi.danger@ynov.com>
2026-07-01 11:02:57 +0000

    modified

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,15 @@ variable "assume_role_arn" {
   description = "ARN du rôle IAM restreint à assumer"
   type        = string
 }
+
+variable "source_bucket_name" {
+  description = "Nom du bucket S3 source pour l'upload des images"
+  type        = string
+  default     = "ynov-iac-2025-source-g3"
+}
+
+variable "destination_bucket_name" {
+  description = "Nom du bucket S3 de destination pour les fichiers PDF"
+  type        = string
+  default     = "ynov-iac-2025-dest-g3"
+}
